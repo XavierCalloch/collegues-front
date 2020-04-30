d313278c8a925c0f90cb87164a7748fccde81ca9
Xavier CALLOCH <callochxavier@gmail.com>
2020-04-30 09:01:48 +0200

    Angular - TP Collègues #7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,18 @@ import { RechercheCollegueParNomComponent } from './recherche-collegue-par-nom/r
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { Collegue2Component } from './collegue2/collegue2.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { GallerieComponent } from './gallerie/gallerie.component';
+import { AProposComponent } from './apropos/apropos.component';
+
+export const ROUTES: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'gallerie', component: GallerieComponent },
+  { path: 'apropos', component: AProposComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'accueil'}
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +27,17 @@ import { Collegue2Component } from './collegue2/collegue2.component';
     CollegueComponent,
     RechercheCollegueParNomComponent,
     Collegue2Component,
+    MenuComponent,
+    AccueilComponent,
+    GallerieComponent,
+    AProposComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
